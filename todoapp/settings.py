e14f31e58dc94e7cf7754577f276f2e5d333a610
David Indzhiev <egniel@yandex.ru>
2018-06-13 04:23:12 +0300

    update settings(add template dir, add staticfiles_dirs)

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'todoapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 AUTH_USER_MODEL = 'users.User'
 
