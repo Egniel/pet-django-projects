4580ef1c7e7fa11bb6890d6a43425d17d6e19b68
David Indzhiev <egniel@yandex.ru>
2018-06-13 04:24:39 +0300

    add custom js and css files(main.js, main.css)

---
+++ static/js/main.js
@@ -0,0 +1,65 @@
+$( document ).ready(function() {
+
+    $(document).on({
+        mouseenter: function(){
+            $(this).addClass('active');
+        },
+        mouseleave: function(){
+            $(this).removeClass('active');
+        }
+    }, '.list-group-item')
+    $(document).on('click', '.btn-outline-success', function(event){
+        $.ajax({
+            url: '/api/v1/todos/' + $(this).attr('data-pk') + '/',
+            type: 'PATCH',
+            data: {
+                done: true,
+            },
+            success: function(data) {
+                $.get('/todo/get-todos/', function(data){
+                    $('#todos').html(data);
+                })
+            },
+        });
+    });
+    $(document).on('click', '.btn-outline-primary', function(event){
+        $.ajax({
+            url: '/api/v1/todos/' + $(this).attr('data-pk') + '/',
+            type: 'PATCH',
+            data: {
+                done: false,
+            },
+            success: function(data) {
+                $.get('/todo/get-todos/', function(data){
+                    $('#todos').html(data);
+                })
+            },
+        });
+    });
+    $(document).on('click', '.btn-outline-warning', function(){
+        $.ajax({
+            url: '/api/v1/todos/' + $(this).attr('data-pk') + '/',
+            type: 'DELETE',
+
+            success: function(data) {
+                $('#todos').html('').load('/todo/get-todos/');
+            }
+        });
+    });
+    $(document).on('submit', '#addTodoForm', function (event) {
+        event.preventDefault();
+        data = {
+            text: $('#addTodo').val(),
+            done: false,
+        };
+        $.ajax({
+            url: '/api/v1/todos/',
+            type: 'POST',
+            data: data,
+            success: function(data) {
+                $('#todos').html('').load('/todo/get-todos/');
+                $('#addTodo').val('');
+            }
+        });
+    });
+});
