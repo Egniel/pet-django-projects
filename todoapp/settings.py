59a95a45101d44882930fcd802ca56e51a7ce126
David Indzhiev <egniel@yandex.ru>
2018-06-18 01:18:26 +0300

    update settings(add staticfile dirs, static root, media root and url), add path to media in debug mode

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -124,7 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'frontend/dist/static'),
+)
+
+STATIC_ROOT = 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
 
--- todoapp/urls.py
+++ todoapp/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
+
 
 from .views import IndexView
 
@@ -24,3 +27,7 @@ urlpatterns = [
     path('api/v1/', include('api.urls')),
     path('todo/', include('todo.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
