4e517653fe6413433fcfbfc9cb6eb74db67964e9
David Indzhiev <egniel@yandex.ru>
2018-09-29 03:12:13 +0300

    remove .venv

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cd frontend && npm run build && cd ..
 echo 'Done...Running npm build'
 
 echo 'Copy vue index.html to templates and rename it to todo_vue.html'
-mv 'frontend/dist/index.html' 'templates/todo_vue.html'
+mv 'frontend/index.html' 'templates/todo_vue.html'
 echo 'Done...Copy index.html'
 
 echo 'Collect static'
@@ -11,9 +11,9 @@ python manage.py collectstatic --noinput
 echo 'Done...Collect static'
 
 echo 'Restart gunicorn'
-systemctl restart gunicorn
+sudo systemctl restart gunicorn
 echo 'Done...Restart gunicorn'
 
 echo 'Restart nginx'
-nginx -t && systemctl restart nginx
+sudo nginx -t && sudo systemctl restart nginx
 echo 'Done...Restart nginx'
